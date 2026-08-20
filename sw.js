cf693f9468e0a9146c80e404d385b8ef41864d51
desimallofficial-cpu <desimall.official@gmail.com>
2026-08-20 22:16:52 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='desimall-v4-20260731';
+const CORE=['./','./index.html','./offline.html','./manifest.webmanifest','./css/style.css','./js/pwa.js','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
+self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;const url=new URL(event.request.url);if(url.origin!==location.origin)return;event.respondWith(fetch(event.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));return res;}).catch(async()=>await caches.match(event.request)||await caches.match('./offline.html')));});
